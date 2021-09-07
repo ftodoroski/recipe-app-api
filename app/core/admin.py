9e538cbc83350976459b1b643e3fcf432005d9da
Filip Todoroski <ftodoroski@ymail.com>
2021-09-06 20:20:35 -0400

    configured postgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -19,7 +19,7 @@ class UserAdmin(BaseUserAdmin):
                 'fields': ('is_active', 'is_staff', 'is_superuser')
             }
         ),
-        (_('Important dates'), {'fields': ('last_login', )})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,4 +1,4 @@
-import time 
+import time
 
 from django.db import connections
 from django.db.utils import OperationalError
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
+        """Handle the command"""
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
@@ -19,6 +20,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available'))
-
-
-        
